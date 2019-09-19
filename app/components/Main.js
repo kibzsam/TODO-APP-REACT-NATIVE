7e14aeb923f960c3e1e7b9be1badc43d278d6096
Mb96-4 <mb96-4@Mb96-4s-iMac.local>
2019-09-19 16:23:23 +0300

    Add feature to Todo App

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,21 +1,22 @@
- /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/**
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow
+*/
 
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import {
-  SafeAreaView,
+  TextInput,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
-} from 'react-native';
+  TouchableOpacity,
 
+} from 'react-native';
+import Note from './Note';
 import {
   Header,
   LearnMoreLinks,
@@ -23,62 +24,113 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+//import { tsConstructorType } from '@babel/types';
 
-const Main = () => {
-  return (
-    <View style={styles.container}> 
-        
-    </View>
-    <View style={styles.header}> 
-        
-    </View>
-  );
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: '',
+    }
+  }
+  render(){
+    let notes = this.state.noteArray.map((val, key) => {
+      return <Note key={key} keyval={key} val={val}
+        deleteMethod={() => this.deleteNote(key)} />
+    });
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>NOTES</Text>
+        </View>
+        <ScrollView style={styles.scrollContainer}>
+          {notes}
+        </ScrollView>
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(noteText) => this.setState({ noteText })}
+            value={this.state.noteText}
+            placeholder='>Note'
+            placeholderTextColor='white'
+            underlineColorAndroid='transparent'>
+          </TextInput>
+        </View>
+        <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  addNote(){
+    if (this.state.noteText){
+      var d = new Date();
+      this.state.noteArray.push({
+        'date':d.getFullYear()+
+        "/" + (d.getMonth()+1)+
+        "/" + d.getDate(),
+        'note': this.state.noteText
+      });
+      this.setState({noteArray:this.state.noteArray})
+      this.setState({noteText:''});
+    }
+  }
 };
 
 const styles = StyleSheet.create({
-  scrollView: { 
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  container:{
-    flex:1,
+  header: {
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  headerText: {
+    color: 'white',
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+    padding: 26,
   },
-  highlight: {
-    fontWeight: '700',
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+
   },
   footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+  }, 
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
+  },
+  addButton: {
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: '#E91E63',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24,
   },
 });
-
-
-
-
 
 export default Main;
